cc9474e779bd1ff768be16ec953fbc41ca203a97
Julia <ibralenko6724809@gmail.com>
2023-12-11 22:15:55 +0300

    add structure

---
+++ src/Components/App.jsx
@@ -0,0 +1,17 @@
+import '../Components/App'
+import Header from './Header/Header'
+import Main from './Main/Main'
+import Footer from './Footer/Footer'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  )
+}
+
+export default App
