e712ea6de93ec24e6040d463f4bb96fa94249add
Julia <ibralenko6724809@gmail.com>
2023-12-21 00:21:31 +0300

    add button in cards to show translate

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,11 +1,19 @@
-import data from '../data.json'
+import style from './Card.module.scss'
+import React, { useState } from 'react'
 
 export default function Card({item}) {
+const [translate, setTranslate] = useState(false)
+
+const showTranslate = () => {
+  setTranslate(!translate)
+}
+
   return (
-    <div>
-<h3>{item.english}</h3>
-<h4>{item.transcription}</h4>
-<p>{item.russian}</p>
+    <div className={style.card}>
+      <h3 className={style.word}>{item.english}</h3>
+      <h3 className={style.transcription}>{item.transcription}</h3>
+      <p className={translate? `${style.word}`: `${style.translate_none}`}>{item.russian}</p>
+      <button className={style.button} onClick={showTranslate}>Показать перевод</button>
     </div>
   )
 
