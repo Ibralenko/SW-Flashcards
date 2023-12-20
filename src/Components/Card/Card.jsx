f9a22d73754c4b4cc6f0b8745777baf0875a5ef6
Julia <ibralenko6724809@gmail.com>
2023-12-21 00:35:19 +0300

    add state to btn in Card (show/hide translate)

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -13,7 +13,7 @@ const showTranslate = () => {
       <h3 className={style.word}>{item.english}</h3>
       <h3 className={style.transcription}>{item.transcription}</h3>
       <p className={translate? `${style.word}`: `${style.translate_none}`}>{item.russian}</p>
-      <button className={style.button} onClick={showTranslate}>Показать перевод</button>
+      <button className={style.button} onClick={showTranslate}>{translate? 'Скрыть перевод' : 'Показать перевод'}</button>
     </div>
   )
 
--- src/Pages/Cards/Cards.jsx
+++ src/Pages/Cards/Cards.jsx
@@ -2,7 +2,6 @@ import data from '../../Components/data.json'
 import Card from '../../Components/Card/Card'
 
 export default function Cards() {
-  
   return (
     <div>
       {data.map((item, index)=> <Card item={item} key={index}/>)}
