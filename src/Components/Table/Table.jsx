166e22a92bb20260fb529d99fdbdf0d28c0a3d8c
Julia <ibralenko6724809@gmail.com>
2023-12-21 00:33:07 +0300

    add func to editCard in 'Table'

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -1,12 +1,30 @@
 import style from './Table.module.scss';
-
+import { useState } from 'react';
 
 export default function(props) {
   const {english, transcription, russian} = props;
+
+  const [editCard, setEditCard] = useState(props.russian)
+
+  const editTranslate = () => {
+    setEditCard(!edit)
+  }
   return (
     <div>
       <table border="0">
-        <tr><td className={style.td}>{english}</td><td className={style.td}>{transcription}</td><td className={style.td}>{russian}</td><td className={style.td } ><button className={style.btn}><img className={style.ico} src='/src/assets/images/edit-ico.png' alt="редактировать" /></button><button  className={style.btn}><img className={style.ico} src='/src/assets/images/delete-ico.png' alt="удалить" /></button></td></tr>
+        <tr>
+        <td className={style.td}>{english}</td>
+        <td className={style.td}>{transcription}</td>
+        <td className={style.td}>{editCard}</td>
+        <td className={style.td }>
+          <button onClick={editTranslate} className={style.btn}>
+            <img className={style.ico} src='/src/assets/images/edit-ico.png' alt="редактировать" />
+          </button>
+          <button  className={style.btn}>
+            <img className={style.ico} src='/src/assets/images/delete-ico.png' alt="удалить" />
+          </button>
+        </td>
+        </tr>
         </table>
     </div>
   )
