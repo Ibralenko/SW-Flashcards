104f29feb837a9883a3793cd8571eaadaf62ddaa
Julia <ibralenko6724809@gmail.com>
2023-12-22 14:48:19 +0300

    add style to the button in card

--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -8,36 +8,26 @@ export default function(props) {
 
   const editTranslate = (event) => {
     setEditCard(event.target.value)
-
   }
 
-  const canceleEditCard = () => {
-   editCard
-  }
-
-  const [saveEdit, setSaveEdit] = useState(russian)
-
-  const saveBtn = () => {
-    setEditCard(!editCard)
-  }
   return (
     <div>
       <table border="0">
         <tr>
         <td className={style.td}>{english}</td>
         <td className={style.td}>{transcription}</td>
-        <td className={style.td}><input type="text" value={saveEdit}/></td>
+        <td className={style.td}><input type="text" value={editCard}/></td>
         <td className={style.td }>
           <button onClick={editTranslate} className={style.btn}>
             <img className={style.ico} src='/src/assets/images/edit-ico.png' alt="редактировать" />
           </button>
-          <button onClick={canceleEditCard } className={editCard? `${style.btn_none}` : `${style.btn}`}>
+          <button className={editCard? `${style.btn_none}` : `${style.btn}`}>
             <img  className={style.ico} src='/src/assets/images/cancel-ico.png' alt="отменить редактирование" />
           </button>
-          <button onClick={saveBtn } className={editCard? `${style.btn_none}` : `${style.btn}`}>
+          <button className={editCard? `${style.btn_none}` : `${style.btn}`}>
             <img className={style.ico} src='/src/assets/images/save-ico.png' alt="сохранить редактирование" />
           </button>
-          <button onClick={canceleEditCard} className={style.btn}>
+          <button  className={style.btn}>
             <img className={style.ico} src='/src/assets/images/delete-ico.png' alt="удалить" />
           </button>
         </td>
