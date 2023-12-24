42c5bec7230f4c6edf4fdc388ece3409217ba668
Julia <ibralenko6724809@gmail.com>
2023-12-24 23:56:32 +0300

    delete the new slider, return old

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,5 +1,5 @@
 import style from "./Card.module.scss";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import data from "../data.json";
 
 export default function Card() {
@@ -9,64 +9,39 @@ export default function Card() {
     setTranslate(!translate);
   };
 
-  // const [index, setIndex] = useState(0);
 
-  // const [count, setCount] = useState(data[index]);
+const [index, setIndex] = useState(0)
 
-  const slider = useRef(null);
-  let position = 0;
-  const [items, setItems] = useState(data);
+const [count, setCount] = useState(data[index])
 
-  const nextHandler = () => {
-    // if (data[index] == undefined) {
-    //   return alert("error");
-    // } else setIndex(index + 1);
-    // setCount(data[index]);
-    position -= 290;
-    console.log(slider.current);
-    slider.current.childNodes.forEach((elem) => {
-      elem.style = `transform: translateX(${position}px)`;
-    });
-  };
+function next () {
+if (data[index] == undefined){
+  return alert('error')
+} else setIndex(index + 1)
+setCount(data[index])
+}
 
-  const prevHandler = () => {
-    // if (index < 0) {
-    //   return alert("error");
-    // } else setIndex(index - 1);
-    // setCount(data[index]);
-    console.log(slider);
-    position += 290;
-    slider.current.childNodes.forEach((elem) => {
-      elem.style = `transform: translateX(${position}px)`;
-    });
-  };
+function previous () {
+  if (index < 0){
+    return alert('error')
+  } else setIndex(index - 1)
+setCount(data[index])
+}
 
   return (
-    <div className={`${style.slider}`}>
-      <div className={style.slider__track} ref={slider}>
-        {data.map((item, index) => (
-          <div className={style.card} key={index} ref={slider}>
-            <h3 className={style.word}>{item.english}</h3>
-            <h3 className={style.transcription}>{item.transcription}</h3>
-            <p
-              className={
-                translate ? `${style.word}` : `${style.translate_none}`
-              }
-            >
-              {item.russian}
-            </p>
-            <button className={style.button} onClick={showTranslate}>
-              {translate ? "Скрыть перевод" : "Показать перевод"}
-            </button>{" "}
-          </div>
-        ))}
+    <>
+      <div className={style.card} key={count}>
+        <button onClick={previous} className={style.left_array}></button>
+        <h3 className={style.word}>{count.english}</h3>
+        <h3 className={style.transcription}>{count.transcription}</h3>
+        <p className={translate ? `${style.word}` : `${style.translate_none}`}>
+          {count.russian}
+        </p>
+        <button className={style.button} onClick={showTranslate}>
+          {translate ? "Скрыть перевод" : "Показать перевод"}
+        </button>
+        <button onClick={next} className={style.right_array}></button>
       </div>
-      <button onClick={prevHandler} className={style.left_array}>
-        left
-      </button>
-      <button onClick={nextHandler} className={style.right_array}>
-        right
-      </button>
-    </div>
+    </>
   );
 }
