5c95572e7d612b504528f218c3e1673047a67ae3
Julia <ibralenko6724809@gmail.com>
2023-12-25 00:07:37 +0300

    add css to the cards

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -31,7 +31,7 @@ setCount(data[index])
   return (
     <>
       <div className={style.card} key={count}>
-        <button onClick={previous} className={style.left_array}></button>
+        <button onClick={previous} className={style.left_arrow}>prev</button>
         <h3 className={style.word}>{count.english}</h3>
         <h3 className={style.transcription}>{count.transcription}</h3>
         <p className={translate ? `${style.word}` : `${style.translate_none}`}>
@@ -40,7 +40,7 @@ setCount(data[index])
         <button className={style.button} onClick={showTranslate}>
           {translate ? "Скрыть перевод" : "Показать перевод"}
         </button>
-        <button onClick={next} className={style.right_array}></button>
+        <button onClick={next} className={style.right_arrow}>next</button>
       </div>
     </>
   );
