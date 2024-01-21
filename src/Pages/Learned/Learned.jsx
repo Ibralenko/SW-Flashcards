c3f2ed24c3106881fa8abdaedc4b2821c8f6f575
Julia <ibralenko6724809@gmail.com>
2024-01-21 19:59:16 +0300

    delete learned cards (they didn't work), need to fix

--- src/Pages/Learned/Learned.jsx
+++ src/Pages/Learned/Learned.jsx
@@ -3,7 +3,7 @@ import LearnedCard from "../../Components/LearndeWords/LearnedCard"
 export default function Learned()  {
   return (
 <>
-<LearnedCard />
+<h1>Здесь будет таблица изученых слов </h1>
 </>
   )
 }
